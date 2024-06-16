59e183aae0eaedbf97ee65ddc91bcb3cd4b9c3e5
fichimura <fabichimura@gmail.com>
2024-06-16 09:08:07 -0300

    created component to reuse in the buttons submit and cancel in the sign in and sign up

---
+++ src/app/ui/submit-cancel/submit-cancel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-cancel',
+  standalone: true,
+  imports: [],
+  templateUrl: './submit-cancel.component.html',
+  styleUrl: './submit-cancel.component.scss',
+})
+export class SubmitCancelComponent {}
