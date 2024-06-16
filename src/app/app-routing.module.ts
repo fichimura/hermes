44635fa8c2d022c94045d30e8cd7877a1df81cd8
fichimura <fabichimura@gmail.com>
2024-06-16 09:07:14 -0300

    add sign in path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'sign-in', component: SignInComponent },
 ];
 
 @NgModule({
