ae11a396f913e33dd3d65ca5fe28e0143420f1cc
fichimura <fabichimura@gmail.com>
2024-06-16 08:45:07 -0300

    sign up page with its form

---
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss',
+})
+export class SignUpComponent {}
