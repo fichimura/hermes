6806bb61ee92e7920e016962c61233f0850414a8
fichimura <fabichimura@gmail.com>
2024-06-16 09:08:54 -0300

    using submitCancel Component to render the buttons

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SubmitCancelComponent } from '../../ui/submit-cancel/submit-cancel.component';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, SubmitCancelComponent],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
 })
