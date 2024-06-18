f698ad25a0b6b8ff3432ea3483a2188e19fe66fa
fichimura <fabichimura@gmail.com>
2024-06-18 16:07:02 -0300

    card Component to display the cards of List Component

---
+++ src/app/ui/card/card.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  subject = input.required<any>();
+}
