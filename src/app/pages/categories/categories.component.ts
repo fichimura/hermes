bb9e00df11988973ee631b97382be292ecab9d23
fichimura <fabichimura@gmail.com>
2024-06-18 21:39:32 -0300

    categories component with list component to list the categories

---
+++ src/app/pages/categories/categories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ListComponent } from '../../ui/list/list.component';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [ListComponent],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.scss',
+})
+export class CategoriesComponent {}
