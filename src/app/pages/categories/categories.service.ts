ba7ce832cfdf25e7fa44dcb4352e91dfff948154
fichimura <fabichimura@gmail.com>
2024-06-18 21:39:52 -0300

    categories service with method to get all categories

---
+++ src/app/pages/categories/categories.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  private readonly CATEGORIES_URL =
+    'https://api.escuelajs.co/api/v1/categories';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getCategories(): Observable<any> {
+    return this.httpClient.get(this.CATEGORIES_URL);
+  }
+}
