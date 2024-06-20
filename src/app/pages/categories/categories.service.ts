17156b26f0b22030945a5be61294bcdd827f1364
fichimura <fabichimura@gmail.com>
2024-06-20 15:52:59 -0300

    remove offset parameter from getCategories method

--- src/app/pages/categories/categories.service.ts
+++ src/app/pages/categories/categories.service.ts
@@ -11,10 +11,8 @@ export class CategoriesService {
 
   constructor(private httpClient: HttpClient) {}
 
-  getCategories(offset?: number): Observable<any> {
-    return this.httpClient.get(
-      `${this.CATEGORIES_URL}/?offset=${offset}&limit=10`
-    );
+  getCategories(): Observable<any> {
+    return this.httpClient.get(this.CATEGORIES_URL);
   }
 
   getProductsOfCategory(
