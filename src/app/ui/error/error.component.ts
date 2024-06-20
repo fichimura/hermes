59a7fda133755180141d66e20afbd84aa2037834
fichimura <fabichimura@gmail.com>
2024-06-20 15:51:15 -0300

    error component t handle errors in components

---
+++ src/app/ui/error/error.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.scss',
+})
+export class ErrorComponent {
+  message = input.required<string>();
+}
