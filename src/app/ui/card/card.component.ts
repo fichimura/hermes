aabef7a586a8130019b902465c6c2dc21eae089a
fichimura <fabichimura@gmail.com>
2024-06-19 23:19:20 -0300

    get first url from parsed JSON to be the card image

--- src/app/ui/card/card.component.ts
+++ src/app/ui/card/card.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,11 +9,21 @@ import { Router } from '@angular/router';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   subject = input.required<any>();
   context = input.required<string>();
+  imageUrl?: string;
 
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    if (
+      this.context() === 'Products' ||
+      this.context() === 'CategoryProducts'
+    ) {
+      this.imageUrl = JSON.parse(this.subject().images)[0];
+    }
+  }
 
   onProductCardClicked(): void {
     this.router.navigate(['/products', this.subject().id]);
